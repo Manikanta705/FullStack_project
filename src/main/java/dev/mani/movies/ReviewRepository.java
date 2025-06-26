79954c23a862bdce12e1267af5d1cc5f9bdb5f8d
KoreManikanta <147417083+Manikanta705@users.noreply.github.com>
2025-06-26 13:56:22 +0530

    Delete src directory

--- src/main/java/dev/mani/movies/ReviewRepository.java
+++
@@ -1,8 +0,0 @@
-package dev.mani.movies;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.bson.types.ObjectId;
-import org.springframework.stereotype.Repository;
-@Repository
-public interface ReviewRepository extends MongoRepository<Review,ObjectId> {
-
-}
